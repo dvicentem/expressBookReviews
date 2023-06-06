0a9431fd082381d3fbbc0c739bea41d8590a2a66
David Vicente <dvicentem@gmail.com>
2023-06-06 21:16:56 +0200

    Task 2 - book by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,8 +17,9 @@ public_users.get('/', function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  //Task 2
+  let isbnBook = books[req.params.isbn]
+  return res.status(200).send(JSON.stringify(isbnBook, null, 2));
 });
 
 // Get book details based on author
