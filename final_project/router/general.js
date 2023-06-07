e9af0b3953717c05eb6d3a738b18115e40e5e202
David Vicente <dvicentem@gmail.com>
2023-06-07 10:00:47 +0200

    Task 3 - books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,8 +24,16 @@ public_users.get('/isbn/:isbn', function (req, res) {
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  //Task 3
+  const author = req.params.author
+  const isbns = Object.keys(books)
+  authorBooks = []
+
+  isbns.forEach((isbn) => {
+    if(books[isbn].author === author) authorBooks.push(books[isbn])
+  })
+
+  return res.status(200).send(JSON.stringify(authorBooks, null, 2));
 });
 
 // Get all books based on title
