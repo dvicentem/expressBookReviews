3d54f734fa17d83d2a1ba3fc52f4426372eb0fae
David Vicente <dvicentem@gmail.com>
2023-06-08 19:57:04 +0200

    Task 10 to 13 - async code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,50 +12,91 @@ public_users.post("/register", (req, res) => {
   if (!username || !password) return res.status(404).json({ message: "User/password invalid" });
   if (isValid(username)) return res.status(404).json({ message: "User already exists" });
 
-  users.push({'username': username, 'password': password})
+  users.push({ 'username': username, 'password': password })
   return res.status(200).send("New user added: " + JSON.stringify(users.filter(user =>
     user.username === username), null, 2));;
 });
 
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
-  return res.status(200).send(JSON.stringify({ books }, null, 2)); // Task 1
+  //Task 10
+  getBooksAsync()
+    .then(response => {
+      return res.status(200).send(response);
+    }).catch(err => {
+      return res.status(404).json({ errorMessage: "Failed to get books" });
+    })
 });
+function getBooksAsync() {
+  return new Promise((resolve) => {
+    resolve(JSON.stringify(books, null, 4));
+  });
+}
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
-  //Task 2
-  let isbnBook = books[req.params.isbn]
-  return res.status(200).send(JSON.stringify(isbnBook, null, 2));
+  //Task 11
+  let isbn = req.params.isbn
+  getDetailIsbnAsync(isbn)
+    .then(response => {
+      return res.status(200).send(response);
+    }).catch(err => {
+      return res.status(404).json({ errorMessage: "Failed to get books" });
+    })
 });
+function getDetailIsbnAsync(isbn) {
+  return new Promise((resolve) => {
+    resolve(JSON.stringify(books[isbn], null, 4));
+  });
+}
+
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  //Task 3
+  //Task 12
   const author = req.params.author
+  getBooksAuthorAsync(author)
+    .then(response => {
+      return res.status(200).send(response);
+    }).catch(err => {
+      return res.status(404).json({ errorMessage: "Failed to get books" });
+    })
+});
+
+function getBooksAuthorAsync(author) {
   const isbns = Object.keys(books)
   authorBooks = []
-
   isbns.forEach((isbn) => {
     if (books[isbn].author === author) authorBooks.push(books[isbn])
   })
-
-  return res.status(200).send(JSON.stringify(authorBooks, null, 2));
-});
+  return new Promise((resolve) => {
+    resolve(JSON.stringify(authorBooks, null, 2));
+  });
+}
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-  //Task 4
+  //Task 13
   const title = req.params.title
+  getBooksTitleAsync(title)
+    .then(response => {
+      return res.status(200).send(response);
+    }).catch(err => {
+      return res.status(404).json({ errorMessage: "Failed to get books" });
+    })
+});
+
+function getBooksTitleAsync(title) {
   const isbns = Object.keys(books)
   titleBooks = []
-
   isbns.forEach((isbn) => {
     if (books[isbn].title === title) titleBooks.push(books[isbn])
   })
+  return new Promise((resolve) => {
+    resolve(JSON.stringify(titleBooks, null, 2));
+  });
+}
 
-  return res.status(200).send(JSON.stringify(titleBooks, null, 2));
-});
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
